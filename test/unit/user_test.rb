24d8463a3d9334261953187b56581c33dabc4c81
Matt Dickens <mpd106@gmail.com>
2013-07-28 22:45:01 +0100

    Adding setup to user tests

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,52 +1,49 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+  def setup
+    @user = FactoryGirl.create :user
+  end
+
   test 'has a valid factory' do
-    user = FactoryGirl.create :user
-    assert_instance_of User, user
+    assert_instance_of User, @user
   end
 
   test 'has a name' do
-    user = FactoryGirl.create :user
-    assert_respond_to user, :name
+    assert_respond_to @user, :name
   end
 
   test 'requires name' do
-    user = FactoryGirl.create :user
-    user.name = nil
-    user.save
-    assert_equal user.errors[:name], ["can't be blank"]
+    @user.name = nil
+    @user.save
+    assert_equal @user.errors[:name], ["can't be blank"]
 
-    user.name = 'Matt'
-    result = user.save
+    @user.name = 'Matt'
+    result = @user.save
     assert result
   end
 
   test 'has an email' do
-    user = FactoryGirl.create :user
-    assert_respond_to user, :email
+    assert_respond_to @user, :email
   end
 
   test 'requires email' do
-    user = FactoryGirl.create :user
-    user.email = nil
-    user.save
-    assert_equal user.errors[:email].first, "can't be blank"
+    @user.email = nil
+    @user.save
+    assert_match "can't be blank", @user.errors[:email].join
 
-    user.email = 'matt@example.com'
-    result = user.save
+    @user.email = 'matt@example.com'
+    result = @user.save
     assert result
   end
 
   test 'email cannot be invalid' do
-    user = FactoryGirl.create :user
-    user.email = 'invalid email'
-    user.save
-    assert_equal user.errors[:email], ["is not an email"]
+    @user.email = 'invalid email'
+    @user.save
+    assert_equal @user.errors[:email], ["is not an email"]
   end
 
   test 'has lists' do
-    user = FactoryGirl.create :user
-    assert_respond_to user, :lists
+    assert_respond_to @user, :lists
   end
 end
