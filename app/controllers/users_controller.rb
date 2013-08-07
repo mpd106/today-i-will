d316f43cc3ae91d13e970d73c05d8ffc80e0f4e4
Matt Dickens <mpd106@gmail.com>
2013-08-07 07:12:44 +0100

    Fleshing out the users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
   respond_to :json
+  before_filter :fetch_user, :except => [:index, :create]
+
+  def fetch_user
+    @user = User.find_by_username params[:id]
+  end
 
   def index
     @users = User.all
@@ -11,12 +16,14 @@ class UsersController < ApplicationController
   end
 
   def show
-    respond_with User.find_by name: params[:id]
+    respond_with @user
   end
 
   def update
+    respond_with @user.update_attributes params[:user]
   end
 
   def destroy
+    respond_with @user.destroy
   end
 end
